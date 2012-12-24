0c414734dfc68961792ad553157c955332d7bbbd
Akhil Wali <akhil.wali.10@gmail.com>
2012-12-24 12:26:00 +0530

    Added org-mode shortcuts

--- init.el
+++ init.el
@@ -137,6 +137,12 @@
                              (add-to-list 'ac-sources 'ac-source-emacs-eclim)
                              (add-to-list 'ac-sources 'ac-source-emacs-eclim-c-dot)))
 
+;; org-mode shortcuts
+(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-cc" 'org-capture)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
 
 ;; my shortcuts
 
