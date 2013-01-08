b30b1f11005e5e4f64c4bd210de0546831beeaf6
Akhil Wali <akhil.wali.10@gmail.com>
2013-01-08 14:09:40 +0530

    Packages upgraded/added/removed. Bumped clojure-mode to 2.0.0. Added clojurescript-mode 0.5. Removed slime. Removed melpa packages.

--- elpa/clojure-mode-2.0.0/clojure-mode-autoloads.el
+++ elpa/clojure-mode-2.0.0/clojure-mode-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 
 
-;;;### (autoloads (clojure-enable-slime-on-existing-buffers clojure-jack-in
-;;;;;;  clojure-mode) "clojure-mode" "clojure-mode.el" (20539 48888))
+;;;### (autoloads (clojure-mode) "clojure-mode" "clojure-mode.el"
+;;;;;;  (20715 53638 0 0))
 ;;; Generated autoloads from clojure-mode.el
 
 (autoload 'clojure-mode "clojure-mode" "\
@@ -21,23 +21,20 @@ if that value is non-nil.
 
 \(fn)" t nil)
 
-(autoload 'clojure-jack-in "clojure-mode" "\
+(put 'clojure-test-ns-segment-position 'safe-local-variable 'integerp)
 
+(put 'clojure-mode-load-command 'safe-local-variable 'stringp)
 
-\(fn)" t nil)
+(add-to-list 'auto-mode-alist '("\\.clj\\'" . clojure-mode))
 
-(autoload 'clojure-enable-slime-on-existing-buffers "clojure-mode" "\
+(add-to-list 'interpreter-mode-alist '("jark" . clojure-mode))
 
-
-\(fn)" t nil)
-
-(add-hook 'slime-connected-hook 'clojure-enable-slime-on-existing-buffers)
-
-(add-to-list 'auto-mode-alist '("\\.clj$" . clojure-mode))
+(add-to-list 'interpreter-mode-alist '("cake" . clojure-mode))
 
 ;;;***
 
-;;;### (autoloads nil nil ("clojure-mode-pkg.el") (20539 48888 451635))
+;;;### (autoloads nil nil ("clojure-mode-pkg.el") (20715 53638 609000
+;;;;;;  0))
 
 ;;;***
 
--- init.el
+++ init.el
@@ -4,9 +4,6 @@
 (add-to-list 'package-archives
     '("marmalade" .
       "http://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives
-  '("melpa" .
-    "http://melpa.milkbox.net/packages/") t)
 (package-initialize)
 
 ;; rainbow parens
