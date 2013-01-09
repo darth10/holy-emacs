5701dffe135c40781c649c0bcbcab900c0da2235
Akhil Wali <akhil.wali.10@gmail.com>
2013-01-09 12:49:03 +0530

    Added emacs manual to fetch-info-docs.sh

--- fetch-info-docs.sh
+++ fetch-info-docs.sh
@@ -4,14 +4,20 @@ mkdir info
 cd info
 echo "Fetching GNU libc manual..."
 wget http://www.gnu.org/software/libc/manual/info/libc-info.tar.gz
+echo "Fetching GNU Emacs manual..."
+wget http://www.gnu.org/software/emacs/manual/info/emacs.info.tar.gz
 echo "Fetching GNU Emacs Lisp manual..."
 wget http://www.gnu.org/software/emacs/manual/info/elisp.info.tar.gz
 echo "Installing GNU libc manual..."
 tar xvf libc-info.tar.gz
 ginstall-info libc.info dir-libc
+echo "Installing GNU Emacs manual..."
+tar xvf emacs.info.tar.gz
+ginstall-info emacs dir-emacs
 echo "Installing GNU Emacs Lisp manual..."
 tar xvf elisp.info.tar.gz
 ginstall-info elisp dir-elisp
 echo "Cleaning up..."
 rm libc-info.tar.gz
+rm emacs.info.tar.gz
 rm elisp.info.tar.gz
