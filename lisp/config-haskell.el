2ad5983144d336b0df07d96e7e5a5a7890688847
Akhil Wali <akhil.wali.10@gmail.com>
2013-02-25 14:31:51 +0530

    Added auto indent to haskell-mode and haskell-cabal-mode

--- lisp/config-haskell.el
+++ lisp/config-haskell.el
@@ -4,6 +4,7 @@
 (require 'ghci-completion)
 
 (defun haskell-bindings ()
+  (local-set-key "\r" 'newline-and-indent)
   (local-set-key (kbd "C-<tab>") 'dabbrev-expand))
 
 (defun load-file-in-inf-haskell ()
@@ -19,11 +20,15 @@
   (local-set-key (kbd "C-x <f10>") 'switch-to-haskell)
   (local-set-key (kbd "C-x <f5>") 'load-file-in-inf-haskell))
 
+(defun configure-haskell-cabal ()
+  (haskell-bindings))
+
 (defun configure-haskell-inf ()
   (ghci-completion-mode)
   (haskell-bindings))
 
 (add-hook 'haskell-mode-hook 'configure-haskell)
 (add-hook 'inferior-haskell-mode-hook 'configure-haskell-inf)
+(add-hook 'haskell-cabal-mode-hook 'configure-haskell-cabal)
 
 (provide 'config-haskell)
