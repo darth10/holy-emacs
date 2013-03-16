5436671024bc1e542962a12e7fa8e521d281ce70
Akhil Wali <akhil.wali.10@gmail.com>
2013-03-16 21:21:09 +0530

    Added js-comint and key binginds for js-mode

--- lisp/config-js.el
+++ lisp/config-js.el
@@ -1,7 +1,19 @@
 ;;; Configuration for JavaScript
 
 (defun configure-js ()
-  (local-set-key "\r" 'newline-and-indent))
+  (local-set-key "\r" 'newline-and-indent)
+  (local-set-key (kbd "C-<f10>") 'run-js)
+  (local-set-key (kbd "C-<f5>") 'js-send-buffer-and-go))
+
+(setq inferior-js-program-command "node")
+(setq inferior-js-mode-hook
+      (lambda ()
+	(ansi-color-for-comint-mode-on)
+	;; Deal with some prompt nonsense
+	(add-to-list
+	 'comint-preoutput-filter-functions
+	 (lambda (output)
+	   (replace-regexp-in-string "\033\\[[0-9]+[GK]" "" output)))))
 
 (add-hook 'js-mode-hook 'configure-js)
 
