30277c9d8f9b4ebc30af92d4d2c995b5ac24a523
Akhil Wali <akhil.wali.10@gmail.com>
2013-03-17 14:56:08 +0530

    Guile enhancements

--- lisp/config-guile.el
+++ lisp/config-guile.el
@@ -7,13 +7,21 @@
   (geiser-compile-current-buffer)
   (geiser-mode-switch-to-repl t))
 
+(defun configure-guile-paredit ()
+  (paredit-mode))
+
 (defun configure-guile ()
-  (paredit-mode)
+  (configure-guile-paredit)
   (local-set-key "\r" 'newline-and-indent)
-  (local-set-key (kbd "C-x <f10>") 'run-guile)
-  (local-set-key (kbd "C-x <f5>") 'guile-load-and-repl)
-  (local-set-key (kbd "C-x <f8>") 'connect-to-guile))
+  (local-set-key (kbd "C-<f10>") 'run-guile)
+  (local-set-key (kbd "C-<f5>") 'guile-load-and-repl)
+  (local-set-key (kbd "C-<f8>") 'connect-to-guile))
+
+(defun configure-guile-inf ()
+  (when (eq major-mode 'geiser-repl-mode)
+    (configure-guile-paredit)))
 
 (add-hook 'scheme-mode-hook 'configure-guile)
+(add-hook 'comint-mode-hook 'configure-guile-inf)
 
 (provide 'config-guile)
