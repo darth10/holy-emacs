c112f4a4a707f5384020ad8d639c3b7572a1a9d4
Akhil Wali <akhil.wali.10@gmail.com>
2013-03-17 14:37:20 +0530

    Elisp enhancements

--- lisp/config-elisp.el
+++ lisp/config-elisp.el
@@ -5,17 +5,21 @@
   (split-window-right)
   (ielm))
 
-(defun configure-elisp ()
+(defun configure-elisp-paredit ()
   (paredit-mode)
-  (turn-on-eldoc-mode)
+  (turn-on-eldoc-mode))
+
+(defun configure-elisp ()
+  (configure-elisp-paredit)
   (local-set-key "\r" 'newline-and-indent)
-  (local-set-key (kbd "C-x <f10>") 'split-and-ielm)
-  (local-set-key (kbd "C-x <f5>") 'eval-buffer))
+  (local-set-key (kbd "C-<f10>") 'split-and-ielm)
+  (local-set-key (kbd "C-<f5>") 'eval-buffer))
 
 (defun configure-elisp-inf ()
-  (local-set-key (kbd "C-x <f5>") 'eval-print-last-sexp))
+  (local-set-key (kbd "C-<f5>") 'eval-print-last-sexp))
 
 (add-hook 'emacs-lisp-mode-hook 'configure-elisp)
+(add-hook 'ielm-mode-hook 'configure-elisp-paredit)
 (add-hook 'lisp-interaction-mode-hook 'configure-elisp-inf)
 
 (provide 'config-elisp)
