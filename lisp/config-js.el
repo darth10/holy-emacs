1bf43f14a002f678e8e6f5dabf07c3cd2ff902c1
Akhil Wali <akhil.wali.10@gmail.com>
2013-03-17 14:19:58 +0530

    Javascript enhancements

--- lisp/config-js.el
+++ lisp/config-js.el
@@ -5,15 +5,15 @@
   (local-set-key (kbd "C-<f10>") 'run-js)
   (local-set-key (kbd "C-<f5>") 'js-send-buffer-and-go))
 
+(defun configure-js-inf ()
+  (ansi-color-for-comint-mode-on)
+  (local-set-key (kbd "TAB") 'dabbrev-expand)
+  (add-to-list 'comint-preoutput-filter-functions   ;; Deal with some prompt nonsense
+	       (lambda (output)
+		 (replace-regexp-in-string "\033\\[[0-9]+[GK]" "" output))))
+
 (setq inferior-js-program-command "node")
-(setq inferior-js-mode-hook
-      (lambda ()
-	(ansi-color-for-comint-mode-on)
-	;; Deal with some prompt nonsense
-	(add-to-list
-	 'comint-preoutput-filter-functions
-	 (lambda (output)
-	   (replace-regexp-in-string "\033\\[[0-9]+[GK]" "" output)))))
+(setq inferior-js-mode-hook 'configure-js-inf)
 
 (add-hook 'js-mode-hook 'configure-js)
 
