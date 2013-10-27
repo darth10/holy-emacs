a9eb8cc038dec674ef2d8af08ed1f7271dfbad61
Akhil Wali <akhil.wali.10@gmail.com>
2013-10-27 23:55:24 +0530

    C# mode tweak for indentation and autocomplete

--- lisp/config/config-csharp.el
+++ lisp/config/config-csharp.el
@@ -3,7 +3,10 @@
 (require 'csharp-mode)
 (require 'config-common)
 
-(defconfig configure-csharp)
+(defconfig configure-csharp
+  (auto-complete-mode)
+  (c-set-style "c#"))
+
 (add-hook 'csharp-mode-hook 'configure-csharp)
 
 (provide 'config-csharp)
