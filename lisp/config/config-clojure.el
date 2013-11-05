abce496d929d9e528fdb490aae6cad0416fcd08c
Akhil Wali <akhil.wali.10@gmail.com>
2013-11-05 21:49:00 +0530

    nrepl.el -> CIDER

--- lisp/config/config-clojure.el
+++ lisp/config/config-clojure.el
@@ -10,28 +10,28 @@
       (if (or (locate-file "lein" exec-path) (locate-file "lein.bat" exec-path))
 	  "lein ritz-in %s" "echo \"lein ritz-in %s\" | $SHELL -l"))
 
-(defun split-and-nrepl-jack-in ()
+(defun split-and-cider-jack-in ()
   "Split window and start nREPL"
   (interactive)
   (split-window-right)
-  (nrepl-jack-in nil))
+  (cider-jack-in nil))
 
 (defun split-and-nrepl ()
   "Split window and start nREPL client"
   (interactive)
   (split-window-right)
-  (nrepl "localhost"
+  (cider "localhost"
 	 (string-to-number (read-from-minibuffer "Port: "))))
 
 (defun load-file-in-nrepl ()
   (interactive)
   (keyboard-escape-quit)
   (split-window-right)
-  (nrepl-load-current-buffer)
-  (nrepl-switch-to-repl-buffer (nrepl-current-ns)))
+  (cider-load-current-buffer)
+  (cider-switch-to-repl-buffer (cider-current-ns)))
 
 (defconfig configure-clojure-keys ()
-  (local-set-key (kbd "C-<f10>") 'split-and-nrepl-jack-in)
+  (local-set-key (kbd "C-<f10>") 'split-and-cider-jack-in)
   (local-set-key (kbd "C-<f5>") 'load-file-in-nrepl)
   (local-set-key (kbd "C-<f8>") 'split-and-nrepl))
 
@@ -45,18 +45,18 @@
   (clojure-test-mode))
 
 (eval-after-load "auto-complete"
-  '(add-to-list 'ac-modes 'nrepl-mode))
+  '(add-to-list 'ac-modes 'cider-mode))
 
 (defun configure-clojure-nrepl-inf ()
   (ac-nrepl-setup)
-  (local-set-key (kbd "C-?") 'nrepl-doc)
+  (local-set-key (kbd "C-?") 'cider-doc)
   (local-set-key (kbd "C-x T") 'clojure-test-run-tests)
   (local-set-key (kbd "C-x t") 'clojure-test-run-test))
 
 (add-hook 'clojure-mode-hook 'configure-clojure)
 (add-hook 'clojure-mode-hook 'configure-lisp)
 (add-hook 'clojure-mode-hook 'configure-clojure-keys)
-(add-hook 'nrepl-repl-mode-hook 'configure-clojure-nrepl)
-(add-hook 'nrepl-interaction-mode-hook 'configure-clojure-nrepl-inf)
+(add-hook 'cider-repl-mode-hook 'configure-clojure-nrepl)
+(add-hook 'cider-mode-hook 'configure-clojure-nrepl-inf)
 
 (provide 'config-clojure)
