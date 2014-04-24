978b60d5437ccd5281613543a51a0e3a51874684
Akhil Wali <akhil.wali.10@gmail.com>
2014-04-25 00:53:05 +0530

    Enable Paredit and Eldoc in eshell

--- lisp/config/config-elisp.el
+++ lisp/config/config-elisp.el
@@ -9,12 +9,17 @@
   (local-set-key (kbd "C-<f5>") 'eval-buffer)
   (local-set-key (kbd "C-c C-k") 'eval-buffer))
 
+(defun configure-interactive-elisp ()
+  (turn-on-eldoc-mode)
+  (paredit-mode t))
+
 (defun configure-elisp-inf ()
   (local-set-key (kbd "C-<f5>") 'eval-print-last-sexp))
 
 (add-hook 'emacs-lisp-mode-hook 'configure-elisp)
 (add-hook 'emacs-lisp-mode-hook 'configure-lisp)
-(add-hook 'ielm-mode-hook 'turn-on-eldoc-mode)
+(add-hook 'ielm-mode-hook 'configure-interactive-elisp)
+(add-hook 'eshell-mode-hook 'configure-interactive-elisp)
 (add-hook 'lisp-interaction-mode-hook 'configure-elisp-inf)
 
 (provide 'config-elisp)
