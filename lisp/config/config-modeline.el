ac1021ce19c787c283dac38ac871e3dd4e6ef254
Akhil Wali <akhil.wali.10@gmail.com>
2014-04-25 01:21:44 +0530

    Fixed highlighting bug in modeline config

--- lisp/config/config-modeline.el
+++ lisp/config/config-modeline.el
@@ -2,12 +2,10 @@
 
 (add-to-list 'mode-line-format
              (quote (:eval (propertize (if sticky-control-mode " :" "  ")
-                                                         'face 'mode-line
-                                                         'help-echo (concat "Sticky control mode " (if sticky-control-mode "enabled" "disabled"))))))
+                                       'help-echo (concat "Sticky control mode " (if sticky-control-mode "enabled" "disabled"))))))
 
 (add-to-list 'mode-line-format
              (quote (:eval (propertize (if god-local-mode "G" " ")
-                        			   'face 'mode-line
                                        'help-echo (concat "God mode " (if god-local-mode "enabled" "disabled"))))))
 
 (provide 'config-modeline)
