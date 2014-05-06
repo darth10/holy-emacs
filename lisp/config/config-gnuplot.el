ad30515f2918beab02e43ad4887198583ebc3563
Akhil Wali <akhil.wali.10@gmail.com>
2014-05-06 22:56:22 +0530

    Revised gnuplot bindings

--- lisp/config/config-gnuplot.el
+++ lisp/config/config-gnuplot.el
@@ -9,9 +9,12 @@
   (local-unset-key (kbd "C-c C-k"))
   (local-set-key (kbd "C-c C-k") 'gnuplot-send-buffer-to-gnuplot)
   (local-set-key (kbd "C-<f5>") 'gnuplot-send-buffer-to-gnuplot)
+  (local-set-key (kbd "C-x C-a C-a") 'gnuplot-send-buffer-to-gnuplot)
+  (local-set-key (kbd "C-x a a") 'gnuplot-send-buffer-to-gnuplot)
   (local-unset-key (kbd "C-c C-z"))
   (local-set-key (kbd "C-c C-z") 'gnuplot-show-gnuplot-buffer)
-  (local-set-key (kbd "C-c k") 'gnuplot-kill-gnuplot-buffer)
+  (local-set-key (kbd "C-<f10>") 'gnuplot-show-gnuplot-buffer)
+  (local-set-key (kbd "C-! C-r") 'gnuplot-show-gnuplot-buffer)
   (local-set-key (kbd "C-?") 'gnuplot-info-at-point))
 
 (add-hook 'gnuplot-mode-hook 'configure-gnuplot)
