10a20538f1937ec1101060b17b05274543258eaa
Akhil Wali <akhil.wali.10@gmail.com>
2014-05-07 20:38:01 +0530

    More Python key bindings

--- lisp/config/config-python.el
+++ lisp/config/config-python.el
@@ -10,9 +10,18 @@
                                           debug-command)))
     (pdb user-debug-command)))
 
+(defun load-file-in-inf-python ()
+  (interactive)
+  (python-shell-send-file (buffer-file-name)))
+
 (defconfig configure-py
+  (local-set-key (kbd "C-<f5>") 'load-file-in-inf-python)
+  (local-set-key (kbd "C-x C-a C-a") 'load-file-in-inf-python)
+  (local-set-key (kbd "C-x a a") 'load-file-in-inf-python)
   (local-set-key (kbd "C-<f10>") 'run-python)
-  (local-set-key (kbd "C-<f11>") 'run-python-debugger))
+  (local-set-key (kbd "C-! C-r") 'run-python)
+  (local-set-key (kbd "C-<f11>") 'run-python-debugger)
+  (local-set-key (kbd "C-! C-d") 'run-python-debugger))
 
 (add-hook 'python-mode-hook 'configure-py)
 
