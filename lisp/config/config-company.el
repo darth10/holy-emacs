abdff4900566b6395cbeedf2cf090eb9c286b614
Akhil Wali <akhil.wali.10@gmail.com>
2016-03-05 22:47:18 +0530

    Updated company-mode binding

--- lisp/config/config-company.el
+++ lisp/config/config-company.el
@@ -3,8 +3,8 @@
 (use-package company
   :ensure t
   :diminish company-mode
-  :bind (("C-' ' a" . global-company-mode)
-         ("C-' C-' C-a" . global-company-mode))
+  :bind (("C-' a" . global-company-mode)
+         ("C-' C-a" . global-company-mode))
   :config
   (global-unset-key (kbd "M-SPC"))
   (bind-key "M-SPC" 'company-manual-begin)
