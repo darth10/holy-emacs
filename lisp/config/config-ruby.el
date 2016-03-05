9712b4abd24160f88d45645ef9f63d2be60fcce4
Akhil Wali <akhil.wali.10@gmail.com>
2016-03-05 06:03:42 +0530

    Migrated ruby-mode

--- lisp/config/config-ruby.el
+++ lisp/config/config-ruby.el
@@ -1,60 +1,54 @@
 ;;; Configuration for Ruby
 
-;; (require 'config-common)
-(require 'rvm)
+(use-package ruby-mode
+  :ensure t
+  :mode ("\\.rb\\'" . ruby-mode))
 
-;; (use-package ruby-mode
-;;   :ensure t
-;;   :config
-;;   )
+(use-package inf-ruby
+  :ensure t
+  :bind (:map ruby-mode-map
+         ("C-! C-r" . run-ruby)
+         ("C-<f10>" . run-ruby)
+         ("C-x C-a C-a" . load-file-in-inf-ruby)
+         ("C-x a a" . load-file-in-inf-ruby)
+         ("C-<f5>" . load-file-in-inf-ruby)
+         ("C-! C-d" . run-ruby-debugger)
+         ("C-<f11>" . run-ruby-debugger))
+  :config
+  (defun load-file-in-inf-ruby ()
+    (interactive)
+    (ruby-load-file (buffer-file-name))
+    (ruby-switch-to-inf (get-buffer "*ruby*")))
+  (defun run-ruby-debugger ()
+    (interactive)
+    (let* ((debug-command (concat  "ruby -r debug " buffer-file-name))
+           (user-debug-command (read-string "Run ruby -r debug (like this): "
+                                            debug-command)))
+      (gud-gdb user-debug-command))))
 
-(defun ruby-insert-end ()
-  "Insert \"end\" at point and reindent current line."
-  (interactive)
-  (insert "end")
-  (ruby-indent-line t)
-  (end-of-line))
+(use-package ruby-end
+  :ensure t
+  :diminish ruby-end-mode)
 
-(defun rinari-run-all-test ()
-  (interactive)
-  (rinari-rake "test"))
+(use-package rinari
+  :ensure t
+  :bind (:map ruby-mode-map
+         ("C-! C-a" . rinari-rake)
+         ("C-<f7>" . rinari-rake))
+  :config
+  (defun rinari-run-all-test ()
+    (interactive)
+    (rinari-rake "test"))
+  (bind-key "C-x T" 'rinari-run-all-test ruby-mode-map))
 
-(defun load-file-in-inf-ruby ()
-  (interactive)
-  (ruby-load-file (buffer-file-name))
-  (ruby-switch-to-inf (get-buffer "*ruby*")))
+(use-package yari
+  :ensure t
+  :bind (:map ruby-mode-map
+         ("C-?" . yari)))
 
-(defun run-ruby-debugger ()
-  (interactive)
-  (let* ((debug-command (concat  "ruby -r debug " buffer-file-name))
-         (user-debug-command (read-string "Run ruby -r debug (like this): "
-                                          debug-command)))
-    (gud-gdb user-debug-command)))
-
-(defconfig configure-ruby
-  (require 'ruby-mode)
-  (require 'ruby-electric)
-  (require 'ido)
-  (require 'rinari)
-  (require 'yari)
-  ;; (smartparens-mode)
-  (local-set-key (kbd "C-?") 'yari)
-  (local-set-key (kbd "C-<f5>") 'load-file-in-inf-ruby)
-  (local-set-key (kbd "C-x C-a C-a") 'load-file-in-inf-ruby)
-  (local-set-key (kbd "C-x a a") 'load-file-in-inf-ruby)
-  (local-set-key (kbd "C-<f10>") 'run-ruby)
-  (local-set-key (kbd "C-! C-r") 'run-ruby)
-  (local-set-key (kbd "C-<f7>") 'rinari-rake)
-  (local-set-key (kbd "C-! C-a") 'rinari-rake)
-  (local-set-key (kbd "C-x T") 'rinari-run-all-test)
-  (local-set-key (kbd "C-<f11>") 'run-ruby-debugger)
-  (local-set-key (kbd "C-! C-d") 'run-ruby-debugger)
-  (ruby-electric-mode t))
-
-(add-to-list 'auto-mode-alist '("\\.rb\\'" . ruby-mode))
-(autoload 'ruby-mode "ruby-mode" "Major mode for editing Ruby code" t)
-(add-hook 'ruby-mode-hook 'configure-ruby)
-
-(rvm-use-default)
+(use-package rvm
+  :ensure t
+  :config
+  (add-hook 'ruby-mode-hook 'rvm-use-default))
 
 (provide 'config-ruby)
