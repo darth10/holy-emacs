07cb7b1819d9420eddee6c6cb2367ed539cb4b07
Akhil Wali <akhil.wali.10@gmail.com>
2016-03-13 21:31:34 +0530

    Migrated SQL config

--- lisp/config/config-sql.el
+++ lisp/config/config-sql.el
@@ -1,22 +1,26 @@
 ;;; Configuration for SQL
 
-;; (require 'config-common)
+(use-package sql
+  :bind (:map sql-mode-map
+         ("C-! C-r" . sql-product-interactive)
+         ("C-<f10>" . sql-product-interactive)
+         ("C-x a a" . sql-send-paragraph)
+         ("C-x C-a C-a" . sql-send-paragraph)
+         ("C-c d" . sql-set-product)
+         ("C-c C-d" . sql-set-product))
+  :config
+  (add-to-list 'process-coding-system-alist '("sqlcmd" . cp850-dos))
+  (setq sql-ms-program "sqlcmd")
+  (setq sql-ms-options nil)
 
-(add-to-list 'process-coding-system-alist '("sqlcmd" . cp850-dos))
-(setq sql-ms-program "sqlcmd")
-(setq sql-ms-options nil)
+  (defun configure-sql ()
+    (sql-set-product 'ms))
 
-(defconfig configure-sql
-  ;; (auto-complete-mode)
-  ;; (smartparens-mode)
-  (sql-set-product 'ms))
+  (defun configure-sql-interactive ()
+    (toggle-truncate-lines t)
+    (setq yas-extra-modes '(sql-mode)))
 
-(defun configure-sql-interactive ()
-  ;; (auto-complete-mode)
-  (toggle-truncate-lines t)
-  (setq yas-extra-modes '(sql-mode)))
-
-(add-hook 'sql-mode-hook 'configure-sql)
-(add-hook 'sql-interactive-mode-hook 'configure-sql-interactive)
+  (add-hook 'sql-mode-hook 'configure-sql)
+  (add-hook 'sql-interactive-mode-hook 'configure-sql-interactive))
 
 (provide 'config-sql)
