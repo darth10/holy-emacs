54e46244d2101ba0f0cfc9dd262e416158ebd5ff
Akhil Wali <akhil.wali.10@gmail.com>
2016-04-27 22:15:03 +0530

    Migrated Python config

--- lisp/config/config-python.el
+++ lisp/config/config-python.el
@@ -1,29 +1,26 @@
 ;;; Configuration for Python
 
-;; (require 'config-common)
+(use-package python
+  ;; :ensure t
+  :mode ("\\.py\\'" . python-mode)
+  :bind (:map python-mode-map
+         ("C-<f5>" . load-file-in-inf-python)
+         ("C-x C-a C-a" . load-file-in-inf-python)
+         ("C-x a a" . load-file-in-inf-python)
+         ("C-<f10>" . run-python)
+         ("C-! C-r" . run-python)
+         ("C-<f11>" . run-python-debugger)
+         ("C-! C-d" . run-python-debugger))
+  :config
+  (defun run-python-debugger ()
+    (interactive)
+    (let* ((debug-command (concat  "pdb " buffer-file-name))
+           (user-debug-command (read-string "Run pdb (like this): "
+                                            debug-command)))
+      (pdb user-debug-command)))
 
-
-(defun run-python-debugger ()
-  (interactive)
-  (let* ((debug-command (concat  "pdb " buffer-file-name))
-         (user-debug-command (read-string "Run pdb (like this): "
-                                          debug-command)))
-    (pdb user-debug-command)))
-
-(defun load-file-in-inf-python ()
-  (interactive)
-  (python-shell-send-file (buffer-file-name)))
-
-(defconfig configure-py
-  ;; (smartparens-mode)
-  (local-set-key (kbd "C-<f5>") 'load-file-in-inf-python)
-  (local-set-key (kbd "C-x C-a C-a") 'load-file-in-inf-python)
-  (local-set-key (kbd "C-x a a") 'load-file-in-inf-python)
-  (local-set-key (kbd "C-<f10>") 'run-python)
-  (local-set-key (kbd "C-! C-r") 'run-python)
-  (local-set-key (kbd "C-<f11>") 'run-python-debugger)
-  (local-set-key (kbd "C-! C-d") 'run-python-debugger))
-
-(add-hook 'python-mode-hook 'configure-py)
+  (defun load-file-in-inf-python ()
+    (interactive)
+    (python-shell-send-file (buffer-file-name))))
 
 (provide 'config-python)
