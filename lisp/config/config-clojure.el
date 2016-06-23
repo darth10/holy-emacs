736f509ec3d4300aca5dbe7c4534b6f345737742
Akhil Wali <akhil.wali.10@gmail.com>
2016-06-23 09:04:35 +0530

    Minor modifications for ClojureScript

--- lisp/config/config-clojure.el
+++ lisp/config/config-clojure.el
@@ -18,10 +18,12 @@
   :diminish clj-refactor-mode
   :config
   (setq cljr-warn-on-eval nil)
+
   (defun configure-clj-refactor ()
     (clj-refactor-mode t)
     (local-unset-key (kbd "C-:"))
     (cljr-add-keybindings-with-prefix "C-c ESC"))
+
   (add-hook 'clojure-mode-hook 'configure-clj-refactor))
 
 (use-package cider
@@ -43,16 +45,21 @@
          ("C-! C-o" . cider-connect)
          ("C-<f8>" . cider-connect)
          ("C-! C-r" . cider-jack-in)
-         ("C-<f10>" . cider-jack-in))
+         ("C-<f10>" . cider-jack-in)
+         :map clojurescript-mode-map
+         ("C-! C-r" . cider-jack-in-clojurescript)
+         ("C-<f10>" . cider-jack-in-clojurescript))
   :config
   (setq cider-auto-select-error-buffer t)
   (setq cider-repl-popup-stacktraces t)
   (setq cider-inject-dependencies-at-jack-in nil)
+
   (defun load-file-in-nrepl ()
     (interactive)
     (cider-load-buffer)
     (cider-repl-set-ns (cider-current-ns))
     (cider-switch-to-repl-buffer))
+
   (use-package eldoc-mode
     :config
     (add-hook 'cider-mode-hook 'turn-on-eldoc-mode)))
@@ -62,6 +69,7 @@
   :config
   (defun config-init-cider-eval-sexp-fu ()
     (require 'cider-eval-sexp-fu))
+
   (add-hook 'cider-mode-hook 'config-init-cider-eval-sexp-fu))
 
 (provide 'config-clojure)
