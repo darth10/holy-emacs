7c6b8d7423343fc1d25f683effcd3eb6761fdfc0
Akhil Wali <akhil.wali.10@gmail.com>
2018-12-09 22:41:28 +1300

    Added godef and gocode integration

--- lisp/config/config-go.el
+++ lisp/config/config-go.el
@@ -1,9 +1,26 @@
 ;;; Configuration for Go
 
+;;; Requires a few Go packages:
+;; $ go get -v github.com/mdempsky/gocode
+;; $ go get -v github.com/rogpeppe/godef
+
 (use-package go-mode
   :ensure t
   :mode ("\\.go\\'" . go-mode)
+  :bind (:map go-mode-map
+              ("M-." . godef-jump))
   :config
-  (add-hook 'before-save-hook 'gofmt-before-save))
+  (add-hook 'before-save-hook 'gofmt-before-save)
+  (use-package company-go
+    :ensure t
+    :bind (:map go-mode-map
+           ("." . company-go-intellisense)
+           ("M-SPC" . company-go))
+    :config
+    (defun company-go-intellisense ()
+      (interactive)
+      (insert ".")
+      (call-interactively 'company-go))))
+
 
 (provide 'config-go)
