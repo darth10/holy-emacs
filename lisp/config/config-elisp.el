e478fe08a2c5d018c8347c15b92a77d6ebc08517
Akhil Wali <akhil.wali.10@gmail.com>
2019-01-16 14:37:04 +1300

    Added interactive compile function for Emacs Lisp

--- lisp/config/config-elisp.el
+++ lisp/config/config-elisp.el
@@ -7,11 +7,18 @@
   (bind-key "C-<f5>" 'eval-print-last-sexp lisp-interaction-mode-map)
   (bind-key "C-c l" 'load-file emacs-lisp-mode-map)
   (bind-key "C-c C-l" 'load-file emacs-lisp-mode-map)
-  (bind-key "C-c k" 'eval-buffer emacs-lisp-mode-map)
-  (bind-key "C-c C-k" 'eval-buffer emacs-lisp-mode-map)
   (bind-key "C-x a a" 'eval-buffer emacs-lisp-mode-map)
   (bind-key "C-x C-a C-a" 'eval-buffer emacs-lisp-mode-map)
-  (bind-key "C-<f5>" 'eval-buffer emacs-lisp-mode-map))
+  (bind-key "C-<f5>" 'eval-buffer emacs-lisp-mode-map)
+
+  (defun +elisp-compile-file ()
+	(interactive)
+	(if (file-exists-p (concat buffer-file-name "c"))
+		(byte-recompile-file buffer-file-name)
+	  (byte-compile-file buffer-file-name)))
+
+  (bind-key "C-c k" '+elisp-compile-file emacs-lisp-mode-map)
+  (bind-key "C-c C-k" '+elisp-compile-file emacs-lisp-mode-map))
 
 (use-package eldoc
   :diminish eldoc-mode
