b34378ba46d49c8f139f06ed4d41bccec11c2fd3
Akhil Wali <akhil.wali.10@gmail.com>
2019-01-24 21:58:32 +1300

    Updated scratch buffer text to display version and copyright

--- lisp/config/config-ui.el
+++ lisp/config/config-ui.el
@@ -28,9 +28,19 @@ For information about GNU Emacs and the GNU system, type C-h C-a.")
     (let* ((face-for-logo 'font-lock-function-name-face)
            (face-for-keys 'font-lock-string-face)
            (face-for-comments 'font-lock-comment-delimiter-face)
+           (version-text (concat
+                          (propertize (format "holy-emacs %s"
+                                              holy-emacs-version)
+                                      'face face-for-logo)
+                          " / "
+                          (propertize (format "GNU Emacs %s"
+                                              emacs-version)
+                                      'face face-for-logo)))
            (help-text (replace-regexp-in-string
                        "C-." (lambda (s) (propertize s 'face face-for-keys))
-                       +theme-scratch-message-help-text)))
+                       (concat +theme-scratch-message-help-text "\n\n"
+                               version-text "\n"
+                               emacs-copyright))))
       (concat
        (replace-regexp-in-string
         "^" (propertize ";; " 'face face-for-comments)
