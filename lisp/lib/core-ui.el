bff3da962b027c3cf2e3539ee1e4dfc66fe32f7e
Akhil Wali <akhil.wali.10@gmail.com>
2019-02-01 21:31:11 +1300

    Moved doom-modeline and cursor logic to core-ui.el

--- lisp/lib/core-ui.el
+++ lisp/lib/core-ui.el
@@ -12,6 +12,10 @@ use any custom theme."
   :type '(repeat symbol)
   :group 'holy-emacs)
 
+(defface core-fringe-highlight-face '((t (:foreground "yellow")))
+  "Face for the fringe bitmaps."
+  :group 'holy-emacs)
+
 (defconst core--default-font "Consolas"
   "Default font.")
 
@@ -34,6 +38,9 @@ To quit a partially entered command, type C-g.
 To quit Emacs, type C-x C-c.
 
 For information about GNU Emacs and the GNU system, type C-h C-a.")
+
+(defvar core--modeline-mode-string ""
+  "Current mode state for `doom-modeline`.")
 
 (defun core--get-scratch-message ()
   (let* ((face-for-logo 'font-lock-function-name-face)
@@ -60,16 +67,72 @@ For information about GNU Emacs and the GNU system, type C-h C-a.")
       help-text)
      "\n\n")))
 
-(defface core-fringe-highlight-face '((t (:foreground "yellow")))
-  "Face for the fringe bitmaps."
-  :group 'holy-emacs)
+(defun core--configure-mode ()
+  (let* ((is-line-overflow (> (current-column) 70))
+         (prev-cur-color (face-background 'cursor))
+         (prev-modeline-color (face-background 'doom-modeline-bar))
+         (is-god-mode (and (boundp 'god-local-mode)
+                           god-local-mode))
+         (cur-color (cond (buffer-read-only "Gray")
+                          (is-line-overflow "IndianRed")
+                          (overwrite-mode "yellow")
+                          (t "green")))
+         (cur-type (cond (buffer-read-only 'box)
+                         ((and overwrite-mode is-god-mode) 'hollow)
+                         ((or is-god-mode overwrite-mode) 'box)
+                         (t 'bar)))
+         (next-mode-string (cond ((and overwrite-mode is-god-mode) "λ")
+                                 (is-god-mode "λ")
+                                 (overwrite-mode "!")
+                                 (t " "))))
+    (progn
+      (unless (eq prev-cur-color cur-color)
+        (set-cursor-color cur-color))
+      (unless (eq prev-modeline-color cur-color)
+        (set-face-attribute 'doom-modeline-bar nil :background cur-color)
+        (doom-modeline-refresh-bars))
+
+      (setq cursor-type cur-type)
+      (setq core--modeline-mode-string next-mode-string))))
 
 (use-package solarized-theme
   :ensure t)
 
+(use-package frame
+  :bind (("C-x C-5 C-0" . delete-frame)
+         ("C-x C-5 C-1" . delete-other-frames)
+         ("C-x C-5 C-2" . make-frame-command)))
+
+(use-package doom-modeline
+  :ensure t
+  :defer t
+  :hook (after-init . doom-modeline-init)
+  :config
+
+  (face-spec-set 'doom-modeline-panel '((t (:inherit mode-line-emphasis))))
+  (face-spec-set 'doom-modeline-buffer-modified '((t (:inherit (warning bold) :background nil))))
+  (face-spec-set 'doom-modeline-inactive-bar '((t (:inherit mode-line-emphasis))))
+
+  ;; modeline on Windows needs a bit more height
+  (let* ((modeline-height (if (core:is-windows-p) 40 34)))
+    (setq doom-modeline-height modeline-height
+          doom-modeline-bar-width 11))
+
+  (doom-modeline-def-segment cur-mode
+    (if (doom-modeline--active)
+        '(" " core--modeline-mode-string " ")
+      "   "))
+  (doom-modeline-def-modeline
+    'main
+    '(workspace-number window-number bar cur-mode matches buffer-info-simple buffer-position selection-info)
+    '(debug buffer-encoding major-mode process vcs checker)))
+
 (progn
-  (face-spec-set 'mode-line '((nil (:box nil :overline "#073642" :underline "#073642"))))
-  (face-spec-set 'mode-line-inactive '((nil (:box nil :background "#073642" :overline "#073642" :underline "#073642"))))
+  (custom-set-faces
+   '(mode-line ((t (:box nil :overline nil :underline nil))))
+   ;; #073642 is for solarized theme only
+   '(mode-line-inactive ((t (:box nil :overline nil :underline nil)))))
+
   (face-spec-set 'show-paren-match '((t (:background "Dodgerblue1" :foreground "white" :weight extra-bold))))
 
   (set-fringe-bitmap-face 'right-triangle 'core-fringe-highlight-face)
@@ -86,6 +149,7 @@ For information about GNU Emacs and the GNU system, type C-h C-a.")
   (menu-bar-mode -1)
   (scroll-bar-mode -1)
   (show-paren-mode 1)
+  (blink-cursor-mode t)
 
   ;;; vars set before loading theme
   (setq solarized-use-variable-pitch nil
@@ -107,7 +171,11 @@ For information about GNU Emacs and the GNU system, type C-h C-a.")
         inhibit-startup-screen t
         split-height-threshold 40
         split-width-threshold nil
+        echo-keystrokes 0.05
+        cursor-in-non-selected-windows nil
         initial-scratch-message (core--get-scratch-message))
+
+  (add-hook 'post-command-hook #'core--configure-mode)
 
   ;; customized font (other than core--default-font) will be
   ;; overwritten if the package-selected-packages custom variable
