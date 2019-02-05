17b63ee8c6ac3b4f2ff37b5246107462a22d16b2
Akhil Wali <akhil.wali.10@gmail.com>
2019-02-06 00:00:12 +1300

    Reorganized functions in core-editor.el and added comments

--- lisp/lib/core-editor.el
+++ lisp/lib/core-editor.el
@@ -2,33 +2,7 @@
 
 (defconst core--scratch-buffer-name "*scratch*")
 
-(defun core/switch-to-scratch-other-frame ()
-  "Switch to *scratch* buffer in a new frame."
-  (interactive)
-  (switch-to-buffer-other-frame core--scratch-buffer-name))
-
-(defun core/switch-to-scratch ()
-  "Switch to *scratch* buffer."
-  (interactive)
-  (switch-to-buffer core--scratch-buffer-name))
-
-(defun core/switch-to-scratch-other-window ()
-  "Switch to *scratch* buffer in a new window."
-  (interactive)
-  (switch-to-buffer-other-window core--scratch-buffer-name))
-
-(defun core/confirm-and-kill-terminal ()
-  "Quit Emacs with a confirmation."
-  (interactive)
-  (when (yes-or-no-p "Quit Emacs? ")
-    (save-buffers-kill-terminal)))
-
-(defun core/find-user-init-file ()
-  "Edit user-init-file in another window."
-  (interactive)
-  (find-file-other-window user-init-file))
-
-(defun core/find-or-run-process (new-buffer-name process-f)
+(defun core:find-or-run-process (new-buffer-name process-f)
   "Switches to or opens up a process using PROCESS-F in
 a new buffer named NEW-BUFFER-NAME."
   (let* ((process-buffer (get-buffer new-buffer-name)))
@@ -42,11 +16,85 @@ a new buffer named NEW-BUFFER-NAME."
              (switch-to-buffer process-buffer))
         (funcall process-f))))
 
-(defun core/list-processes-and-switch ()
-  "Show all processes in a new window."
+(defun core:move-line (n)
+  "Move the current line up or down by N lines."
+  (setq col (current-column))
+  (beginning-of-line) (setq start (point))
+  (end-of-line) (forward-char) (setq end (point))
+  (let ((line-text (delete-and-extract-region start end)))
+    (forward-line n)
+    (insert line-text)
+    ;; restore point to original column in moved line
+    (forward-line -1)
+    (forward-char col)))
+
+(defun core:move-region (start end n)
+  "Move the current region up or down by N lines."
+  (let ((line-text (delete-and-extract-region start end)))
+    (forward-line n)
+    (let ((start (point)))
+      (insert line-text)
+      (setq deactivate-mark nil)
+      (set-mark start))))
+
+(defun core/kill-ring-save-line (beg end flash)
+  "Copy current line to kill ring."
+  (interactive (if (use-region-p)
+           (list (region-beginning) (region-end) nil)
+         (list (line-beginning-position)
+               (line-beginning-position 2) 'flash)))
+  (kill-ring-save beg end)
+  (when flash
+    (save-excursion
+      (if (equal (current-column) 0)
+      (goto-char end)
+    (goto-char beg)))))
+
+(defun core/kill-line-utils-init ()
+  "Sets alternate kill/copy key bindings."
+  ;; M-w
+  (global-set-key [remap kill-ring-save] 'core/kill-ring-save-line)
+  ;; C-w
+  (put 'kill-region 'interactive-form
+       '(interactive
+         (if (use-region-p)
+             (list (region-beginning) (region-end))
+           (list (line-beginning-position) (line-beginning-position 2))))))
+
+(defun core/confirm-and-kill-terminal ()
+  "Quit Emacs with a confirmation."
   (interactive)
-  (list-processes)
-  (other-window 1))
+  (when (yes-or-no-p "Quit Emacs? ")
+    (save-buffers-kill-terminal)))
+
+(defun core/find-user-init-file ()
+  "Edit user-init-file in another window."
+  (interactive)
+  (find-file-other-window user-init-file))
+
+(defun core/match-paren (arg)
+  "Go to the matching paren if the cursor is on a paren."
+  (interactive "p")
+  (cond ((looking-at "\\s\(") (forward-list 1) (backward-char 1))
+        ((looking-at "\\s\)") (forward-char 1) (backward-list 1))
+        (t (message "%s" "No parenthesis under cursor!"))))
+
+;;; Interactive window and switching functions
+
+(defun core/switch-to-scratch ()
+  "Switch to *scratch* buffer."
+  (interactive)
+  (switch-to-buffer core--scratch-buffer-name))
+
+(defun core/switch-to-scratch-other-window ()
+  "Switch to *scratch* buffer in a new window."
+  (interactive)
+  (switch-to-buffer-other-window core--scratch-buffer-name))
+
+(defun core/switch-to-scratch-other-frame ()
+  "Switch to *scratch* buffer in a new frame."
+  (interactive)
+  (switch-to-buffer-other-frame core--scratch-buffer-name))
 
 ;; TODO fix bug in this function
 (defun core/resize-window (key)
@@ -82,13 +130,6 @@ a new buffer named NEW-BUFFER-NAME."
           (eq wind-key ?n))    (windmove-down))
      (t nil))))
 
-(defun core/match-paren (arg)
-  "Go to the matching paren if the cursor is on a paren."
-  (interactive "p")
-  (cond ((looking-at "\\s\(") (forward-list 1) (backward-char 1))
-        ((looking-at "\\s\)") (forward-char 1) (backward-list 1))
-        (t (message "%s" "No parenthesis under cursor!"))))
-
 (defun core/delete-single-window (&optional window)
   "Close window WINDOW and kill its buffer."
   (interactive)
@@ -100,86 +141,49 @@ a new buffer named NEW-BUFFER-NAME."
         (delete-frame)
       (delete-window (selected-window)))))
 
-(defun core/kill-ring-save-line (beg end flash)
-  "Copy current line to kill ring."
-  (interactive (if (use-region-p)
-           (list (region-beginning) (region-end) nil)
-         (list (line-beginning-position)
-               (line-beginning-position 2) 'flash)))
-  (kill-ring-save beg end)
-  (when flash
-    (save-excursion
-      (if (equal (current-column) 0)
-      (goto-char end)
-    (goto-char beg)))))
+;;; Interactive process functions
 
-(defun core/kill-line-utils-init ()
-  "Sets alternate kill/copy key bindings."
-  ;; M-w
-  (global-set-key [remap kill-ring-save] 'core/kill-ring-save-line)
-  ;; C-w
-  (put 'kill-region 'interactive-form
-       '(interactive
-     (if (use-region-p)
-         (list (region-beginning) (region-end))
-       (list (line-beginning-position) (line-beginning-position 2))))))
+(defun core/list-processes-and-switch ()
+  "Show all processes in a new window."
+  (interactive)
+  (list-processes)
+  (other-window 1))
 
 (defun core/find-or-run-shell ()
   "Switches to or opens up a new shell."
   (interactive)
-  (core/find-or-run-process
+  (core:find-or-run-process
    "*shell*"
    (lambda () (shell))))
 
 (defun core/find-or-run-eshell ()
   "Switches to or opens up a new eshell."
   (interactive)
-  (core/find-or-run-process
+  (core:find-or-run-process
    "*eshell*"
    (lambda () (eshell "new"))))
 
-(defun core/move-line (n)
-  "Move the current line up or down by N lines."
-  (interactive "p")
-  (setq col (current-column))
-  (beginning-of-line) (setq start (point))
-  (end-of-line) (forward-char) (setq end (point))
-  (let ((line-text (delete-and-extract-region start end)))
-    (forward-line n)
-    (insert line-text)
-    ;; restore point to original column in moved line
-    (forward-line -1)
-    (forward-char col)))
+;;; Interactive line and region movement functions
 
 (defun core/move-line-up (n)
   "Move the current line up by N lines."
   (interactive "p")
-  (core/move-line (if (null n) -1 (- n))))
+  (core:move-line (if (null n) -1 (- n))))
 
 (defun core/move-line-down (n)
   "Move the current line down by N lines."
   (interactive "p")
-  (core/move-line (if (null n) 1 n)))
-
-(defun core/move-region (start end n)
-  "Move the current region up or down by N lines."
-  (interactive "r\np")
-  (let ((line-text (delete-and-extract-region start end)))
-    (forward-line n)
-    (let ((start (point)))
-      (insert line-text)
-      (setq deactivate-mark nil)
-      (set-mark start))))
+  (core:move-line (if (null n) 1 n)))
 
 (defun core/move-region-up (start end n)
   "Move the current line up by N lines."
   (interactive "r\np")
-  (core/move-region start end (if (null n) -1 (- n))))
+  (core:move-region start end (if (null n) -1 (- n))))
 
 (defun core/move-region-down (start end n)
   "Move the current line down by N lines."
   (interactive "r\np")
-  (core/move-region start end (if (null n) 1 n)))
+  (core:move-region start end (if (null n) 1 n)))
 
 (defun core/move-line-region-up (start end n)
   "Move the current region or line up by N lines."
