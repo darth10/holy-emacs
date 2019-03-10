c73daf541fc9fbd2aad975580094632b5c8c6baf
Akhil Wali <akhil.wali.10@gmail.com>
2019-03-10 13:47:59 +1300

    Moved projectile files into cache directory

--- lisp/config/config-projectile.el
+++ lisp/config/config-projectile.el
@@ -5,6 +5,9 @@
   :defer 2
   :bind (:map projectile-mode-map
 		 ("C-c p" . projectile-command-map))
+  :init
+  (setq projectile-cache-file (concat core-var-cache-dir-full-path "projectile/cache")
+        projectile-known-projects-file (concat core-var-cache-dir-full-path "projectile/bookmarks.eld"))
   :config
   (projectile-mode t)
   (use-package helm-projectile
