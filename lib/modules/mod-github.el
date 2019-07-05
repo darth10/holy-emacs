1807b6fe0855cbacf2bee1aad0243f937112fee2
Akhil Wali <akhil.wali.10@gmail.com>
2019-07-05 23:16:06 +1200

    Added forge and github-review packages

--- lib/modules/mod-github.el
+++ lib/modules/mod-github.el
@@ -8,4 +8,24 @@
   :ensure t
   :defer 5)
 
+(use-package github-review
+  :ensure t
+  :after magit
+  :config
+  (transient-append-suffix 'magit-merge "i"
+    '("y" "Review pull request" github-review-forge-pr-at-point)))
+
+(use-package forge
+  :ensure t
+  :after magit
+  :config
+  (setq forge-database-file (concat core-var-cache-dir-full-path
+                                    "forge-database.sqlite")
+        ;; forge-pull-notifications fails for a large number of notifications
+        forge-pull-notifications nil)
+  (transient-append-suffix 'magit-branch "w"
+    '("y" "pull request" forge-checkout-pullreq))
+  (transient-append-suffix 'magit-branch "W"
+    '("Y" "from pull request" forge-branch-pullreq)))
+
 (provide 'mod-github)
