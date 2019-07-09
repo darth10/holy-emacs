b39d136e2deab714cdfebfdf42de5d929c07f9e0
Akhil Wali <akhil.wali.10@gmail.com>
2019-07-09 22:18:47 +1200

    Fixed minor naming

--- lib/modules/mod-lang-elisp.el
+++ lib/modules/mod-lang-elisp.el
@@ -5,11 +5,11 @@
          (:repl-start . core/find-or-run-eshell)
          (:eval-buffer . eval-buffer)
          (:load-file . load-file)
-         (:compile-file . +elisp-compile-file)
+         (:compile-file . +elisp/compile-file)
          :map lisp-interaction-mode-map
          (:eval-buffer . eval-print-last-sexp))
   :config
-  (defun +elisp-compile-file ()
+  (defun +elisp/compile-file ()
 	(interactive)
 	(core:compile-file buffer-file-name)))
 
