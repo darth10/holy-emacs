6b869539aee8e99d9f4c501b2dbbd5fc76ec8ab7
Akhil Wali <akhil.wali.10@gmail.com>
2019-07-09 22:15:05 +1200

    [mod-lang-clojure] Used :lang extensions

--- lib/modules/mod-lang-clojure.el
+++ lib/modules/mod-lang-clojure.el
@@ -10,64 +10,50 @@
   :ensure t
   :after clojure-mode
   :config
-  (setq cljr-warn-on-eval nil)
-
-  (defun configure-clj-refactor ()
+  (defun +clojure/configure-clj-refactor ()
     (clj-refactor-mode t)
     (local-unset-key (kbd "C-:"))
     (cljr-add-keybindings-with-prefix "C-c ESC"))
 
-  (add-hook 'clojure-mode-hook 'configure-clj-refactor))
+  (setq cljr-warn-on-eval nil)
+  (add-hook 'clojure-mode-hook #'+clojure/configure-clj-refactor))
 
 (use-package cider
   :ensure t
   :after clojure-mode
-  :bind (:map cider-mode-map
-         ("C-x a a" . load-file-in-nrepl)
-         ("C-x C-a C-a" . load-file-in-nrepl)
-         ("C-<f5>" . load-file-in-nrepl)
-         ("C-c C-k" . cider-load-buffer)
-         ("C-c k" . cider-load-buffer)
-         ("C-c C-l" . cider-load-file)
-         ("C-c l" . cider-load-file)
-         ("C-x T" . cider-test-run-ns-tests)
-         ("C-x t" . cider-test-run-test)
-         ("C-?" . cider-doc)
-         :map cider-repl-mode-map
-         ("C-x T" . cider-test-run-ns-tests)
-         ("C-x t" . cider-test-run-test)
-         ("C-?" . cider-doc)
-         :map clojure-mode-map
-         ("C-! C-o" . cider-connect)
-         ("C-<f8>" . cider-connect)
-         ("C-! C-r" . cider-jack-in)
-         ("C-<f10>" . cider-jack-in)
+  :lang (:map clojure-mode-map
+         (:repl-start . cider-jack-in)
          :map clojurescript-mode-map
-         ("C-! C-r" . cider-jack-in-clojurescript)
-         ("C-<f10>" . cider-jack-in-clojurescript))
+         (:repl-start . cider-jack-in-cljs)
+         :map cider-mode-map
+         (:eval-buffer . +clojure/eval-buffer-and-switch-ns)
+         (:load-file . cider-load-file)
+         (:test-file . cider-test-run-ns-tests)
+         (:test-all . cider-test-run-project-tests))
   :config
-  (setq cider-auto-select-error-buffer t)
-  (setq cider-repl-popup-stacktraces t)
-  (setq cider-inject-dependencies-at-jack-in nil)
-
-  (defun load-file-in-nrepl ()
+  (defun +clojure/eval-buffer-and-switch-ns ()
     (interactive)
     (cider-load-buffer)
     (cider-repl-set-ns (cider-current-ns))
-    (cider-switch-to-repl-buffer)))
+    (cider-switch-to-repl-buffer))
+
+  (setq cider-auto-select-error-buffer t)
+  (setq cider-repl-popup-stacktraces t)
+  (setq cider-inject-dependencies-at-jack-in nil))
 
 (use-package eldoc
   :after (clojure-mode cider)
   :config
-  (add-hook 'cider-mode-hook 'eldoc-mode)
-  (add-hook 'cider-repl-mode-hook 'eldoc-mode))
+  (add-hook 'cider-mode-hook #'eldoc-mode)
+  (add-hook 'cider-repl-mode-hook #'eldoc-mode))
 
 (use-package cider-eval-sexp-fu
   :ensure t
   :after (clojure-mode cider)
   :config
-  (defun config-init-cider-eval-sexp-fu ()
+  (defun +clojure/config-init-cider-eval-sexp-fu ()
     (require 'cider-eval-sexp-fu))
-  (add-hook 'cider-mode-hook 'config-init-cider-eval-sexp-fu))
+
+  (add-hook 'cider-mode-hook #'+clojure/config-init-cider-eval-sexp-fu))
 
 (provide 'mod-lang-clojure)
