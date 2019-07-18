afa350c3b53d80cb2bd341898843b2b1273cb0c1
Akhil Wali <akhil.wali.10@gmail.com>
2019-07-18 23:07:23 +1200

    Bumped to version 0.2.4

--- lib/core/core.el
+++ lib/core/core.el
@@ -30,7 +30,7 @@
   "An opinionated and extensible Emacs configuration."
   :group 'emacs)
 
-(defconst holy-emacs-version "0.2.3"
+(defconst holy-emacs-version "0.2.4"
   "Version of holy-emacs.")
 
 (defconst core-lib-path "lib/core/"
