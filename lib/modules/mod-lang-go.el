fd4654c999efb7fb9f7d3d34c440767fbaafdc55
Akhil Wali <akhil.wali.10@gmail.com>
2019-07-24 22:09:40 +1200

    [mod-lang-go] Use :lang extension

--- lib/modules/mod-lang-go.el
+++ lib/modules/mod-lang-go.el
@@ -1,4 +1,4 @@
-;;; Configuration for Go
+;;; mod-lang-go.el --- Configuration for Go          -*- lexical-binding: t; -*-
 
 ;;; Requires a few Go packages:
 ;; $ go get -v github.com/mdempsky/gocode
@@ -7,21 +7,23 @@
 (use-package go-mode
   :ensure t
   :mode ("\\.go\\'" . go-mode)
-  :bind (:map go-mode-map
-         ("M-." . godef-jump))
+  :lang (:map go-mode-map
+         (:find-definition . godef-jump))
   :config
   (setq gofmt-show-errors 'echo)
-  (add-hook 'before-save-hook 'gofmt-before-save)
-  (add-hook 'go-mode-hook 'flycheck-mode)
-  (use-package company-go
-    :ensure t
-    :bind (:map go-mode-map
-           ("." . company-go-complete)
-           ("M-SPC" . company-go))
-    :config
-    (defun company-go-complete ()
-      (interactive)
-      (insert ".")
-      (call-interactively 'company-go))))
+  (add-hook 'before-save-hook #'gofmt-before-save)
+  (add-hook 'go-mode-hook #'flycheck-mode))
+
+(use-package company-go
+  :ensure t
+  :after go-mode
+  :bind (:map go-mode-map
+         ("." . +go/company-go-complete)
+         ("M-SPC" . company-go))
+  :config
+  (defun +go/company-go-complete ()
+    (interactive)
+    (insert ".")
+    (call-interactively 'company-go)))
 
 (provide 'mod-lang-go)
