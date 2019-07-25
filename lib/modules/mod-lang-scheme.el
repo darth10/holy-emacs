f78c2d88a95c4b4ea9a95de838eec931a4e58872
Akhil Wali <akhil.wali.10@gmail.com>
2019-07-25 20:16:39 +1200

    [mod-lang-scheme] Use :lang extension

--- lib/modules/mod-lang-scheme.el
+++ lib/modules/mod-lang-scheme.el
@@ -1,24 +1,20 @@
-;;; Configuration for Scheme
+;;; mod-lang-scheme.el --- Configuration for Scheme  -*- lexical-binding: t; -*-
 
 (use-package geiser
   :ensure t
-  :bind (:map scheme-mode-map
-         ("C-<f10>" . run-geiser)
-         ("C-! C-r" . run-geiser)
-         ("C-<f5>" . scheme-load-and-repl)
-         ("C-x C-a C-a" . scheme-load-and-repl)
-         ("C-x a a" . scheme-load-and-repl)
-         ("C-c C-l" . scheme-load-current-file)
-         ("C-c l" . scheme-load-current-file))
-
+  :lang (:map scheme-mode-map
+         (:repl-start . run-geiser)
+         :map geiser-mode-map
+         (:find-definition . geiser-edit-symbol-at-point)
+         (:eval-buffer . +scheme/load-and-repl)
+         (:load-file . +scheme/load-current-file))
   :config
-
-  (defun scheme-load-and-repl ()
+  (defun +scheme/load-and-repl ()
     (interactive)
     (geiser-compile-current-buffer)
     (geiser-mode-switch-to-repl t))
 
-  (defun scheme-load-current-file ()
+  (defun +scheme/load-current-file ()
     (interactive)
     (geiser-load-file buffer-file-name)))
 
