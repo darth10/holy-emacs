921761b617d7907b9458c93c40c229ab53ca108e
Akhil Wali <akhil.wali.10@gmail.com>
2019-07-29 22:21:19 +1200

    [mod-lang-python] Use :lang extension

--- lib/modules/mod-lang-python.el
+++ lib/modules/mod-lang-python.el
@@ -1,25 +1,21 @@
-;;; Configuration for Python
+;;; mod-lang-python.el --- Configuration for Python  -*- lexical-binding: t; -*-
 
 (use-package python
-  ;; :ensure t
   :mode ("\\.py\\'" . python-mode)
-  :bind (:map python-mode-map
-         ("C-<f5>" . load-file-in-inf-python)
-         ("C-x C-a C-a" . load-file-in-inf-python)
-         ("C-x a a" . load-file-in-inf-python)
-         ("C-<f10>" . run-python)
-         ("C-! C-r" . run-python)
-         ("C-<f11>" . run-python-debugger)
-         ("C-! C-d" . run-python-debugger))
+  :lang (:map python-mode-map
+         (:repl-start . run-python)
+         (:eval-buffer . +python/load-file)
+         (:load-file . +python/load-file)
+         (:debugger . +python/run-debugger))
   :config
-  (defun run-python-debugger ()
+  (defun +python/run-debugger ()
     (interactive)
     (let* ((debug-command (concat  "pdb " buffer-file-name))
            (user-debug-command (read-string "Run pdb (like this): "
                                             debug-command)))
       (pdb user-debug-command)))
 
-  (defun load-file-in-inf-python ()
+  (defun +python/load-file ()
     (interactive)
     (python-shell-send-file (buffer-file-name))))
 
