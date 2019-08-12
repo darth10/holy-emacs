e7b35c197c2b3a369eda0b15c7202bcea9315676
Akhil Wali <akhil.wali.10@gmail.com>
2019-08-12 21:17:56 +1200

    [mod-lang-csharp] Use :lang :comp

--- lib/modules/mod-lang-csharp.el
+++ lib/modules/mod-lang-csharp.el
@@ -17,26 +17,19 @@
   :ensure t
   :after csharp-mode
   :commands (omnisharp-mode omnisharp-start-omnisharp-server)
-  :lang (:map omnisharp-mode-map
+  :lang (:comp (omnisharp-mode . company-omnisharp)
+         :map omnisharp-mode-map
          (:find-definition . omnisharp-go-to-definition)
          (:find-usages . omnisharp-find-usages)
          (:format-buffer . omnisharp-code-format-entire-file)
          (:compile-file . omnisharp-solution-errors))
   :bind (:map omnisharp-mode-map
-         ("M-SPC" . company-omnisharp)
-         ("." . +csharp/company-omnisharp-complete)
          ("C-." . omnisharp-run-code-action-refactoring)
          ("C--" . pop-tag-mark))
   :init
   (setq omnisharp-cache-directory
         (concat core-var-cache-dir-full-path "omnisharp/"))
   :config
-  (defun +csharp/company-omnisharp-complete ()
-    (interactive)
-    (insert ".")
-    (when omnisharp-mode
-      (call-interactively #'company-omnisharp)))
-
   (add-hook 'omnisharp-mode-hook #'flycheck-mode)
   (add-hook 'csharp-mode-hook #'omnisharp-mode))
 
