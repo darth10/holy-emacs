99e0a3eb87527ea8d480d44684ad355e94513636
Akhil Wali <akhil.wali.10@gmail.com>
2019-08-13 22:59:48 +1200

    [mod-lang-php] Use :lang extension and add company-php

--- lib/modules/mod-lang-php.el
+++ lib/modules/mod-lang-php.el
@@ -1,69 +1,38 @@
-;;; Configuration for PHP
+;;; mod-lang-php.el --- Configuration for PHP        -*- lexical-binding: t; -*-
+
+;; To debug PHP scripts, start debugger using M-x geben and run:
+;; php -S localhost:8081
+;;     -d xdebug.remote_enable=on \
+;;     -d xdebug.remote_host=127.0.0.1 \
+;;     -d xdebug.remote_port=9000 \
+;;     -d xdebug.remote_handler=dbgp \
+;;     -d xdebug.idekey=geben \
+;;     -d xdebug.remote_autostart=On \
+;;     public/index.php
 
 (use-package php-mode
   :ensure t
   :mode ("\\.php\\'" . php-mode))
 
+(use-package company-php
+  :ensure t
+  :after php-mode
+  :lang (:comp (php-mode . company-ac-php-backend)))
+
 (use-package geben
   :ensure t
-  :defer 5
+  :after php-mode
+  :lang (:map php-mode-map
+         (:debugger . geben)
+         :map geben-mode-map
+         (:debug-set-break . geben-set-breakpoint-line)
+         (:debug-remove-break . geben-unset-breakpoint-line)
+         (:debug-step-over . geben-step-over)
+         (:debug-step-into . geben-step-into)
+         (:debug-step-out . geben-step-out)
+         (:debug-continue . geben-run))
   :init
   (setq geben-temporary-file-directory
-        (concat core-var-cache-dir-full-path "geben/"))
-  :config
-
-  (defun configure-geben (s)
-
-    (bind-key config-key-gud-break1 'geben-set-breakpoint-line geben-mode-map)
-    (bind-key config-key-gud-break2 'geben-set-breakpoint-line geben-mode-map)
-    (bind-key config-key-gud-break3 'geben-set-breakpoint-line geben-mode-map)
-
-    (bind-key config-key-gud-remove1 'geben-unset-breakpoint-line geben-mode-map)
-    (bind-key config-key-gud-remove2 'geben-unset-breakpoint-line geben-mode-map)
-    (bind-key config-key-gud-remove3 'geben-unset-breakpoint-line geben-mode-map)
-
-    (bind-key config-key-gud-next1 'geben-step-over geben-mode-map)
-    (bind-key config-key-gud-next2 'geben-step-over geben-mode-map)
-    (bind-key config-key-gud-next3 'geben-step-over geben-mode-map)
-
-    (bind-key config-key-gud-step1 'geben-step-into geben-mode-map)
-    (bind-key config-key-gud-step2 'geben-step-into geben-mode-map)
-    (bind-key config-key-gud-step3 'geben-step-into geben-mode-map)
-
-    (bind-key config-key-gud-finish1 'geben-step-out geben-mode-map)
-    (bind-key config-key-gud-finish2 'geben-step-out geben-mode-map)
-    (bind-key config-key-gud-finish3 'geben-step-out geben-mode-map)
-
-    (bind-key config-key-gud-cont1 'geben-run geben-mode-map)
-    (bind-key config-key-gud-cont2 'geben-run geben-mode-map)
-    (bind-key config-key-gud-cont3 'geben-run geben-mode-map))
-
-  (add-hook 'geben-session-enter-hook 'configure-geben)
-
-  (use-package php-mode
-    :config
-
-    (defun php-debug-sentinel (process event)
-      (when (= 0 (process-exit-status process))
-        (call-interactively #'geben-end)))
-
-    (defun php-debug ()
-      (interactive)
-      (call-interactively #'geben)
-      (let* ((php-debug-command (concat
-                                 "XDEBUG_CONFIG='idekey=php-54' php "
-                                 (buffer-file-name)))
-             (php-debug-process (start-process
-                                 "php-debug-process"
-                                 "*php-debug-process-output*"
-                                 "/bin/sh" "-c"
-                                 php-debug-command)))
-        (set-process-sentinel php-debug-process 'php-debug-sentinel)))
-
-    (bind-key "C-! C-r" 'php-debug php-mode-map)
-    (bind-key "C-<f10>" 'php-debug php-mode-map)
-
-    (bind-key "C-! C-d" 'geben php-mode-map)
-    (bind-key "C-<f11>" 'geben php-mode-map)))
+        (concat core-var-cache-dir-full-path "geben/")))
 
 (provide 'mod-lang-php)
