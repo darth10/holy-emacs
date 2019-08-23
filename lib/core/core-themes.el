fc2974f1ded65afbcae6d1998d974e7312aa0439
Akhil Wali <akhil.wali.10@gmail.com>
2019-08-23 23:06:50 +1200

    Updated custom-safe-themes for solarized, zenburn, monokai

--- lib/core/core-themes.el
+++ lib/core/core-themes.el
@@ -6,12 +6,12 @@
 (custom-set-variables
  '(custom-safe-themes
    (quote
-    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" ;; solarized-dark
-     "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" ;; solarized-light
+    ("0598c6a29e13e7112cfbc2f523e31927ab7dce56ebb2016b567e1eff6dc1fd4f" ;; solarized-dark
+     "d91ef4e714f05fff2070da7ca452980999f5361209e679ee988e3c432df24347" ;; solarized-light
      "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" ;; spacemacs-dark
      "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" ;; spacemacs-light
-     "d057f0430ba54f813a5d60c1d18f28cf97d271fd35a36be478e20924ea9451bd" ;; zenburn
-     "bd7b7c5df1174796deefce5debc2d976b264585d51852c962362be83932873d9" ;; monokai
+     "a7051d761a713aaf5b893c90eaba27463c791cd75d7257d3a8e66b0c8c346e77" ;; zenburn
+     "a2cde79e4cc8dc9a03e7d9a42fabf8928720d420034b66aecc5b665bbf05d4e9" ;; monokai
      "2f4f50d98073c01038b518066840638455657dc91dd1a225286d573926f36914" ;; heroku
      "f11e219c9d043cbd5f4b2e01713c2c24a948a98bed48828dc670bd64ae771aa1" ;; clues
      "9b35c097a5025d5da1c97dba45fed027e4fb92faecbd2f89c2a79d2d80975181" ;; flatland
